f50ad4fa197da75529c6dc1ccfd6fc77d2c62d7e
Benjamin <benjamindeltour22@gmail.com>
2023-11-27 23:41:09 +0100

    ins: get all movies from the json file and display the title of every movies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import data from '../../public/data/movie.json'
+
 export default function Home() {
+  const movies = data.Filmotech_Films.Film
   return (
     <main>
       <h1>MovieStream</h1>
+      {
+        movies.map((movie)=>{
+          return(
+            <p>{movie.TitreVF}</p>
+          )
+        })
+      }
     </main>
   )
 }

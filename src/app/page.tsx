e1008b25966f8924bfb9dacd831b405004ed92a7
Benjamin <benjamindeltour22@gmail.com>
2023-12-02 18:13:20 +0100

    upd: update the movies and add sorting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 import { Card } from '@/components/card'
 import data from '../../public/data/movie.json'
-import Image from 'next/image'
 
 export default function Home() {
   const movies = data.Filmotech_Films.Film
+  movies.sort((a, b) => {
+      const champA = a.TitreVF.toUpperCase(); // Convertir en majuscules pour ignorer la casse
+      const champB = b.TitreVF.toUpperCase(); // Convertir en majuscules pour ignorer la casse
+
+      if (champA < champB) {
+          return -1; // Retourne un nombre négatif si a précède b
+      }
+      if (champA > champB) {
+          return 1; // Retourne un nombre positif si b précède a
+      }
+      return 0; // Retourne 0 si les deux sont égaux
+  });
   return (
     <main>
       <header>

f8bddb7ea6fc2ef71311028c18e00b01de881347
Benjamin <benjamindeltour22@gmail.com>
2023-12-20 16:36:51 +0100

    ins: change the size of one card when you click on it

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { useState } from 'react'
 
 export function Card(props: {
     title: string,
@@ -6,12 +7,21 @@ export function Card(props: {
      annee: string,
      categ: string
 }) {
+    const [display, setDisplay] = useState('')
     const image = props.image
     const title = props.title
     const annee = props.annee
     const categ = props.categ
+
+    const handleDisplay = () => {
+      if(display)
+        setDisplay('')
+      if(!display)
+        setDisplay('active')
+    }
+    console.log(display)
   return (
-    <div className='card'>
+    <div className={`card ${display}`} onClick={handleDisplay}>
         <Image src={'/affiches/'+image+'.JPG'} alt='image' width={300} height={450}/>
         <h1 className='card_title'>{ title }</h1>
         <div className='card_desc'>

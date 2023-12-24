9a3206175e196b3a234e0cf3764fb62560a2628a
Benjamin <benjamindeltour22@gmail.com>
2023-12-24 14:25:03 +0100

    upd: add animation to infos display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,18 @@
 import { Card } from '@/components/card'
 import { Button } from '@/components/button';
 import data from '../../public/data/movie.json'
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import autoAnimate from '@formkit/auto-animate';
+
 
 export default function Home() {
 
   const [display, setDisplay] = useState(false)
+  const parent = useRef(null)
+  
+  useEffect(() => {
+    parent.current && autoAnimate(parent.current)
+  }, [parent])
 
 
   const movies = data.Filmotech_Films.Film
@@ -35,33 +42,34 @@ export default function Home() {
       </header>
       <section>
         <Button title='Quelques informations en plus !' onClick={handleDisplay}/>
-        {
-          display && 
-          <div className='infos'>
-            <p>Suite à quelques demandes sur les films que je possède voici un site répertoriant tous les films que j'ai et que je peux vous transmettre.</p>
-            <p>Pour informations : </p>
-            <ul>
-              <li>Le site est en constante évolution et donc recevra des mises à jour afin de l'améliorer.</li>
-              <li>La liste de films sera mise à jour à chaque fois que j'ajouterais un nouveau film dans mon disque dur.</li>
-              <li>Vous pouvez à tout moment me demander un film qui n'ai pas dans la liste.</li>
-              <li>Pour le moment, je peux vous transmettre les films via google drive, swissTransfer (mail) ou en physique.</li>
-              <li>Suivant mes disponibilités, vous pouvez recevoir un film au maximum 2/3 jours après la demande.</li>
-              <li>Au minimum, un film sera "livré" en 2h.</li>
-              <li>Les films que je télécharge sont, sauf exception, en minimum 1080p(environ 4/5Go par film) et maximum 4K(entre 5 et 80Go le film)</li>
-              <li>Si les films sont trop gros, pour diverses raisons, ils seront retélécharger pour mieux convenir aux besoins.</li>
-              <li>Je suis preneur de tous les retours que vous pouvez me faire (sur le site, sur les films, ect).</li>
-            </ul>
-            <p>Fonctionnalités à venir : </p>
-            <ul>
-              <li>Affichage des détails des films.</li>
-              <li>Meilleurs rangements des films (une série de films comme mission impossible, par exemple, sera rangée dans l'ordre des films et pas dans l'ordre alphabétique).</li>
-              <li>Ajouter une pagination à la page pour n'afficher qu'une dizaine de films par pages.</li>
-              <li>Ajout d'un filtre pour affiner la recherche d'un film.</li>
-              <li>Ajout de la recherche d'un film précis.</li>
-              <li>Meilleurs partage des films (surprise surprise).</li>
-            </ul>
-          </div>
-        }
+        <div  ref={parent}>
+          { display && 
+            <div className='infos'>
+              <p>Suite à quelques demandes sur les films que je possède voici un site répertoriant tous les films que j'ai et que je peux vous transmettre.</p>
+              <p>Pour informations : </p>
+              <ul>
+                <li>Le site est en constante évolution et donc recevra des mises à jour afin de l'améliorer.</li>
+                <li>La liste de films sera mise à jour à chaque fois que j'ajouterais un nouveau film dans mon disque dur.</li>
+                <li>Vous pouvez à tout moment me demander un film qui n'ai pas dans la liste.</li>
+                <li>Pour le moment, je peux vous transmettre les films via google drive, swissTransfer (mail) ou en physique.</li>
+                <li>Suivant mes disponibilités, vous pouvez recevoir un film au maximum 2/3 jours après la demande.</li>
+                <li>Au minimum, un film sera "livré" en 2h.</li>
+                <li>Les films que je télécharge sont, sauf exception, en minimum 1080p(environ 4/5Go par film) et maximum 4K(entre 5 et 80Go le film)</li>
+                <li>Si les films sont trop gros, pour diverses raisons, ils seront retélécharger pour mieux convenir aux besoins.</li>
+                <li>Je suis preneur de tous les retours que vous pouvez me faire (sur le site, sur les films, ect).</li>
+              </ul>
+              <p>Fonctionnalités à venir : </p>
+              <ul>
+                <li>Affichage des détails des films.</li>
+                <li>Meilleurs rangements des films (une série de films comme mission impossible, par exemple, sera rangée dans l'ordre des films et pas dans l'ordre alphabétique).</li>
+                <li>Ajouter une pagination à la page pour n'afficher qu'une dizaine de films par pages.</li>
+                <li>Ajout d'un filtre pour affiner la recherche d'un film.</li>
+                <li>Ajout de la recherche d'un film précis.</li>
+                <li>Meilleurs partage des films (surprise surprise).</li>
+              </ul>
+            </div>
+          }
+        </div>
       </section>
       <section className="content">
         {

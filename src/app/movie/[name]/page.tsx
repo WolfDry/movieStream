db27d11ee358aa020278ffe7ae82b54b25268700
Benjamin <benjamindeltour22@gmail.com>
2024-02-03 16:59:05 +0100

    upd: add a little bit of style and more information to the movie page

--- src/app/movie/[name]/page.tsx
+++ src/app/movie/[name]/page.tsx
@@ -3,22 +3,58 @@ import "../../../../public/assets/css/style.css"
 import data from '../../../../public/data/movie.json'
 import Header from "@/components/header"
 
-interface props{
-    params: {name: string}
+import '../../../../public/assets/css/movie.css'
+
+interface props {
+  params: { name: string }
 }
 
-export default function Page({params}: props) {
+export default function Page({ params }: props) {
   const movies = data.Filmotech_Films.Film
   const movie = movies.filter((m) => m.Slug === params.name)[0]
-  return(
+  return (
     <main>
       <Header />
       <section className="movie">
         <div className="basic-infos">
-          <Image src={'/affiches/'+movie.ID+'.JPG'} alt='image' width={300} height={450}/>
+          <div>
+            <Image src={'/affiches/' + movie.ID + '.JPG'} alt='image' width={525} height={787.5} />
+          </div>
+          <div>
+            <p>
+              Genre : {movie.Genre}
+            </p>
+          </div>
+          <div>
+            <p>
+              Réalisateur : {movie.Realisateurs}
+            </p>
+            <p>
+              Acteurs : {movie.Acteurs}
+            </p>
+          </div>
         </div>
         <div className="main-infos">
-          
+          <div>
+            <h2>{movie.TitreVF}</h2>
+            <p>{movie.Annee}</p>
+          </div>
+          <div>
+            <p>
+              {movie.Synopsis}
+            </p>
+          </div>
+          <div>
+            <p>
+              Qualité : {movie.Support}
+            </p>
+            <p>
+              Langues : {movie.Langues}
+            </p>
+            <p>
+              Sous-titre : {movie.SousTitres}
+            </p>
+          </div>
         </div>
       </section>
     </main>
